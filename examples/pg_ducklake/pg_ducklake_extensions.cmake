41dd68b36d0c65142bdcca3d529aca051074d313
qsliu <qsliu2017@outlook.com>
2026-06-02 11:03:18 +0800

    load postgres scanner

--- examples/pg_ducklake/pg_ducklake_extensions.cmake
+++ examples/pg_ducklake/pg_ducklake_extensions.cmake
@@ -8,6 +8,12 @@
 
 duckdb_extension_load(json)
 duckdb_extension_load(icu)
+duckdb_extension_load(postgres_scanner
+    DONT_LINK
+    GIT_URL https://github.com/duckdb/duckdb-postgres
+    GIT_TAG c89234f0b1985f4ee0f52f16e742a1ab2d4ae4f0
+    SUBMODULES database-connector
+)
 duckdb_extension_load(ducklake
     DONT_LINK
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/third_party/ducklake
